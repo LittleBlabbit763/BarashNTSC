0e4f418d8037811e4dd04e933e02dc19cb0e9c91
LittleBlabbit763 <155322131+LittleBlabbit763@users.noreply.github.com>
2026-08-17 19:47:06 +0300

    Delete BarashNTSC_publish directory

--- BarashNTSC_publish/hook_ffmpeg.py
+++
@@ -1,4 +0,0 @@
-﻿import os, sys
-if getattr(sys, "frozen", False):
-    paths = [os.path.dirname(sys.executable), sys._MEIPASS]
-    os.environ["PATH"] = os.pathsep.join(paths) + os.pathsep + os.environ.get("PATH", "")
